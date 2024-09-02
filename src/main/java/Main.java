5812cb5154332ec362e6bf23c829eb769a9b6775
Victor Fabris <victor.fabris.santos@escola.pr.gov.br>
2024-09-02 12:02:22 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // Fazer um programa que lê um valor inteiro N e depois N números inteiros.Ao final, mostra a soma dos N números lidos
+
+  for(int i = 0; i < 5; i++  ) {
+    System.out.println(i);
+  }
+  
+    for(int j= 4; j >= 0; j--) {
+      System.out.println(j);
+    }
+
+    // for( início ; condição; incremento) {
+    // comandos
+    // }
+
+    sc.close(); 
+    }
+}
+  
+  
